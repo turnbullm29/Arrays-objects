3a9b02d2b9e27b156be4aa37fdd0ea34801ea4df
Malik Turnbull <Malik@maliks-mbp.home>
2020-07-15 17:22:44 -0400

    [loops]

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ console.log(favoritenumber);
 
 var teams = ['patriots', 'giants', 'saints', 'panthers'];
 console.log(teams);
+
+// While loops
+var number = 10;
+var otherNumber = 1;
+
+while (otherNumber < number) {
+    console.log(otherNumber);
+    //othernNumber +=1;
+    otherNumber = otherNumber + 1; //
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber); // 10, 11, 12
+    myOtherNumber += 1; // 11, 12, 13
+}
