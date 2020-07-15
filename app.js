e964d6b1e235efe318ad4faf40c2dade900ad535
Malik Turnbull <Malik@maliks-mbp.home>
2020-07-15 17:05:35 -0400

    add teams

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = 'malik';
+console.log(name);
+
+var mylocation = 'North Carolina';
+console.log(mylocation);
+
+var favoritenumber = 29;
+console.log(favoritenumber);
+
+var teams = ['patriots', 'giants', 'saints', 'panthers'];
+console.log(teams);
